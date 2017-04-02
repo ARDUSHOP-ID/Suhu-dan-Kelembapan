2a81dca452dc3e2021e0481cf362747d09e43ee5
ARDUSHOP-ID <ardushop.id@gmail.com>
2017-04-02 22:06:27 +0700

    Add files via upload
    Menampilkan Nilai Sensor pada Serial Monitor

---
+++ DHT11_Serial_Monitor.ino
@@ -0,0 +1,57 @@
+/*************************************************************
+  This example shows how value can be pushed from Arduino to
+  Serial Monitor        
+                                                                        
+   ;;`  .;;;;;  ;;;;:  ;;` `;;  ;;;;  ;;  :;.  ;;;;`  ;;;;:   ;;  ;;;;, 
+  ,;;;  .;:.;;, ;;:;;; ;;` `;; ;; ,;: ;;  :;. ;;;:;;  ;;.;;.  ;;  ;;:;;,
+  ;;;;  .;: :;, ;;  ;; ;;` `;; ;;;,   ;;,,;;. ;;  ,;, ;; ,;,  ;;  ;;  ;;
+ `;:`;, .;;;;;  ;;  ;; ;;` `;;  ;;;;, ;;;;;;.`;;  .;: ;;;;;   ;;  ;;  ;;
+ :;;;;; .;:.;;  ;;  ;; ;;` `;; `. `;; ;;  :;. ;;  :;, ;;``    ;;  ;;  ;;
+ ;;;;;; .;: ;;. ;;:;;: .;;:;;. ;;..;; ;;  :;. ;;;:;;  ;;   ;; ;;  ;;:;;
+ 
+  WARNING :
+  For this example you'll need library & Arduino part :
+    Adafruit DHT sensor libraries:
+    https://github.com/adafruit/Adafruit_Sensor
+    https://github.com/adafruit/DHT-sensor-library
+
+  Need Arduino Part :
+  https://www.tokopedia.com/ardushopid
+  or Whatsapp Only 082111545939
+  Find Me on FB :
+  https://id-id.facebook.com/people/Ardushop-Id/100015723427957
+  
+  Pin DHT >> Pin Arduino
+    VCC   >>    +5V
+    DATA  >>    Pin 6 Digital
+    NC    >>    Tidak dipakai
+    GND   >>    GND
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#include <DHT.h>  //menyertakan library DHT kedalam program
+
+#define DHTPIN 6  //kita pasang sensor pada pin 6 digital
+
+#define DHTTYPE DHT11 //kita menggunakan jenis sensor DHT11, ubah jika kamu gunakan sensor lain seperti DHT22 (AM2302) atau DHT21 (AM2301)
+//#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
+//#define DHTTYPE DHT21   // DHT 21, AM2301
+
+DHT dht(DHTPIN, DHTTYPE); //deklarasi pin sensor dengan jenis sensor yang dipilih
+
+void setup() {
+  dht.begin(); //program komunikasi atau setup untuk sensor DHT
+  Serial.begin(9600); //program komunikasi atau setup untuk serial monitor dan kecepatan komunikasi (baudrate)
+}
+
+void loop() {
+  float kelembapan = dht.readHumidity(); //menyimpan nilai kelembapan pada variabel kelembapan
+  float suhu = dht.readTemperature(); //menyimpan nilai suhu pada variabel suhu
+  delay(200); //mengatur jeda waktu pembacaan sensor selama 200 milidetik
+  
+  Serial.print(kelembapan); //menampilkan nilai kelembapan pada Serial Monitor
+  Serial.print("%"); //Simbol persen satuan kelembapan
+  Serial.print(" "); //menambahkan spasi
+  Serial.print(suhu); //menampilkan nilai suhu pada Serial Monitor
+  Serial.println("*C"); //Satuan Derajat Suhu
+}
